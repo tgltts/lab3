35be1884ec2d8b24edb3bd9c4c53cb5938fa2376
tgltts <g82651504@gmail.com>
2025-08-26 22:42:49 -0300

    Update ex3.c

--- ex3.c
+++ ex3.c
@@ -3,41 +3,41 @@
 #include <time.h>
 
 void sorteio(int *intervalo) {
-    srand(time(NULL));  // Inicializa a semente do gerador de números aleatórios
+    srand(time(NULL));  
     *intervalo = 1 + rand() % 100;  // Sorteia um número entre 1 e 100
 }
 
 void palpites(int intervalo) {
-    int palpite, tentativas = 0;
+    int palpite;
     
     do {
         printf("Seu palpite: ");
         scanf("%d", &palpite);
         
-        // Valida se o palpite está dentro do intervalo permitido
+        
         if (palpite < 1 || palpite > 100) {
             printf("Valor inválido, digite novamente!\n");
         } else {
-            tentativas++;
+            
             if (palpite == intervalo) {
-                printf("Parabéns!!! Você acertou em %d tentativas.\n", tentativas);
-                break;  // Sai do loop quando o usuário acerta
+                printf("Parabéns!!! Você acertou em %d tentativas.\n",);
+                break;  
             } else if (palpite < intervalo) {
                 printf("Você chutou muito baixo! Tente novamente.\n");
             } else {
                 printf("Você chutou muito alto! Tente novamente.\n");
             }
         }
-    } while (1);  // Loop até acertar
+    } while (1); 
 }
 
 int main() {
     int intervalo;
     
-    sorteio(&intervalo);  // Chama a função sorteio e passa o endereço da variável 'intervalo'
+    sorteio(&intervalo);  
     printf("Número escolhido, tente adivinhar o número...\n");
     
-    palpites(intervalo);  // Chama a função de palpites com o número sorteado
+    palpites(intervalo); 
     
     return 0;
 }
